a77ebdd58631639ea1267672b35e63034854df65
Shenoy <54286073+shenoy@users.noreply.github.com>
2021-09-05 13:42:22 +0100

    made new folder in windows 10

---
+++ target/generated-sources/annotations/guru/springfamework/api/v1/mapper/CategoryMapperImpl.java
@@ -0,0 +1,29 @@
+package guru.springfamework.api.v1.mapper;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-09-04T16:48:53+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
+)
+@Component
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public CategoryDTO categoryToCategoryDTO(Category category) {
+        if ( category == null ) {
+            return null;
+        }
+
+        CategoryDTO categoryDTO = new CategoryDTO();
+
+        categoryDTO.setId( category.getId() );
+        categoryDTO.setName( category.getName() );
+
+        return categoryDTO;
+    }
+}
---
+++ target/generated-sources/annotations/guru/springfamework/api/v1/mapper/VendorMapperImpl.java
@@ -0,0 +1,41 @@
+package guru.springfamework.api.v1.mapper;
+
+import guru.springfamework.api.v1.model.VendorDTO;
+import guru.springfamework.domain.Vendor;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-09-04T16:48:52+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
+)
+@Component
+public class VendorMapperImpl implements VendorMapper {
+
+    @Override
+    public VendorDTO vendorToVendorDTO(Vendor vendor) {
+        if ( vendor == null ) {
+            return null;
+        }
+
+        VendorDTO vendorDTO = new VendorDTO();
+
+        vendorDTO.setName( vendor.getName() );
+
+        return vendorDTO;
+    }
+
+    @Override
+    public Vendor vendorDTOtoVendor(VendorDTO vendorDTO) {
+        if ( vendorDTO == null ) {
+            return null;
+        }
+
+        Vendor vendor = new Vendor();
+
+        vendor.setName( vendorDTO.getName() );
+
+        return vendor;
+    }
+}
